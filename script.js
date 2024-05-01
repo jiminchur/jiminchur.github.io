96ec56dfc1166a5095c8b76e8f58076c9647e3ab
jiminchur <ttlcc13@naver.com>
2024-05-01 21:48:58 +0900

    test

--- script.js
+++ script.js
@@ -1,52 +1,237 @@
-/*
-Style Stage is a modern CSS showcase styled by community contributions.
-Learn more at https://stylestage.dev
+const nav = document.querySelector("#nav")
+const marker = document.querySelector(".marker")
+const menus = document.querySelectorAll(".nav__menu > li > a")
+const sections = document.querySelectorAll(".section-flag");
+const overlay = document.querySelector("#overlay")
+const bigPhoto = document.querySelectorAll(".gallery__big_photo")
+const thumbnails = document.querySelectorAll(".gallery__thumbnail")
+const body = document.querySelector("body");
+const slide = document.querySelector("#slide");
+let slides = document.querySelectorAll("#slide>li")
+let timerId=0;
+let photoIndex=0;
+const photoCount = bigPhoto.length;
+const duration = 400;
+let bullet = 0;
 
-This pen is loaded with the source html and styles to get you started creating your own stylesheet to contribute.
 
-Updated: 7/9/2020
-*/
-const marker=document.querySelector(".marker");
+function bulletClassReset() {
+    bullet.forEach(a => {
+        a.classList.remove("on")
+    })
 
-//nav의 marker의 길이와 위치를 설정하는 함수
-//A function to set the length and position of the nav marker.
-function setMarker(e) {
+}
+function bulletIndex() {
+    let index = photoIndex + bullet.length;
+    index %= bullet.length;
+    bullet[index].classList.add("on");
+}
+// bullet 생성하는 함수
+function createBullets() {
+    const bullets = document.createElement("ul");
+    bullets.setAttribute("id","bullets");
+    overlay.appendChild(bullets);
+    slides.forEach((slide,index) => {
+        const a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.innerHTML=`${index}`;
+        const li = document.createElement("li")
+        li.appendChild(a);
+        bullets.appendChild(li);
+    })
+    return bullet = document.querySelectorAll("#bullets > li > a");
+}
+createBullets();
+
+// 갤러리 모달창 이벤트
+thumbnails.forEach(item => {
+    item.addEventListener("click",(e)=>{
+        e.preventDefault();
+        body.classList.add("scroll_hidden")
+        overlay.style.display = "block";
+        // 썸네일 원본 사진 링크와 갤러리 슬라이드 이미지 소스 링크 연결
+        for (let i=0; i<thumbnails.length; i++) {
+            var photo = thumbnails[i].lastElementChild;
+            bigPhoto[i].src = photo.href
+        };
+        });
+        document.querySelector("button.close").addEventListener("click", ()=> {
+            overlay.style.display = "none";
+            body.classList.remove("scroll_hidden")
+        
+    })
+
+})
+
+// 슬라이드 버튼 클릭 이벤트
+document.querySelector("#next_btn").addEventListener("click", nextSlideImage)
+document.querySelector("#prev_btn").addEventListener("click", prevSlideImage)
+
+
+// 다음 사진으로 슬라이드
+function nextSlideImage() {
+    photoIndex++;
+    photoIndex %= photoCount;
+    slide.style.left = "-100%";
+    slide.style.transition = duration+"ms";
+    window.setTimeout(()=>{ 
+        slide.appendChild(slide.firstElementChild);
+        slide.removeAttribute("style");
+    },duration)
+    bulletClassReset();
+    bulletIndex()
+}
+// 이전 사진으로 슬라이드
+function prevSlideImage() {
+    photoIndex --;
+    photoIndex %= photoCount;
+    slide.insertBefore(slide.lastElementChild,slide.firstChild);
+    slide.style.left = "-100%";
+    slide.style.transition = "0ms";
+    window.setTimeout(function(){ 
+        slide.style.left = 0;
+        slide.style.transition = duration+"ms";
+    },duration/2)
+    bulletClassReset();
+    bulletIndex()
+}
+
+//bullet을 클릭하면 해당하는 번호의 이미지로 슬라이드 되는 함수
+bullet.forEach((a,index)=>{
+    a.addEventListener("click", (e)=>{
+        e.preventDefault();
+        const clickedIndex = index;
+        let step = clickedIndex - photoIndex;
+        photoIndex = clickedIndex;
+        bulletClassReset();
+        a.classList.add("on");
+        slides = document.querySelectorAll("#slide>li");
+        let slidesArr = [...slides];
+
+        if (step>0) {
+            let sliceSlides = slidesArr.slice(undefined,step);
+            slide.style.transition = duration+"ms";
+            slide.style.left=step * -100+"%";
+            window.setTimeout(()=>{ 
+                slide.removeAttribute("style");
+                slide.append(...sliceSlides);
+            },duration)
+        
+        }else {
+            sliceSlides = slidesArr.slice(step);
+            slide.prepend(...sliceSlides);
+            slide.style.left = step * 100 + "%";
+            window.setTimeout(()=>{ 
+                slide.style.left = 0;
+                slide.style.transition = duration+"ms";
+            },)
+        }
+    })
+})
+//썸네일 클릭하면 해당되는 사진으로 점프
+thumbnails.forEach((item,index)=>{
+    item.addEventListener("click", (e)=>{
+        e.preventDefault();
+        const clickedIndex = index;
+        let step = clickedIndex - photoIndex;
+        photoIndex = clickedIndex;
+        bulletClassReset();
+        bullet[index].classList.add("on");
+        slides = document.querySelectorAll("#slide>li");
+        let slidesArr = [...slides];
+
+        if (step>0) {
+            let sliceSlides = slidesArr.slice(undefined,step);
+            slide.append(...sliceSlides);
+
+        
+        }else {
+            sliceSlides = slidesArr.slice(step);
+            slide.prepend(...sliceSlides);
+        }
+    })
+})
+
+
+// nav의 인디케이터(marker)의 길이와 위치를 맞추는 함수
+function indicator(e) {
     marker.style.left = e.offsetLeft+"px";
     marker.style.width = e.offsetWidth+"px";
 }
 
-const sections = document.querySelectorAll("section");
-const menus = document.querySelectorAll(".nav__menu > li > a")
-
-//스크롤 위치에 따라 해당하는 menu의 색깔과 마커가 달라짐
-//The color and marker of the corresponding menu change according to the scroll position
-
-window.addEventListener("scroll",()=>{
-    //현재 영역의 id값
-    //id of the current section
-    let current=""
-
-    sections.forEach(section=>{
-        //각 section의 top 위치(절대적 위치)
-        // The top of each section (absolute)
-        const sectionTop = window.scrollY + section.getBoundingClientRect().top;
-
-        //누적된 스크롤이 section의 top위치에 도달했거나 section의 안에 위치할 경우
-        //When the accumulated scroll reaches the top of the section or is located inside the section
-        if(window.scrollY >= sectionTop) {
+// 스크롤 위치에 따라 해당하는 nav 메뉴의 색깔이 바뀜 & 섹션 등장 애니메이션
+window.addEventListener("scroll", ()=> {
+    let current="";
+    
+    sections.forEach(section => {
+        const sectionTop = window.pageYOffset + section.getBoundingClientRect().top
+        const sectionHeight = section.clientHeight;
+        var height = document.body.clientHeight - (window.scrollY + window.innerHeight)
+        const contactBox = document.getElementById("contact")
+        
+        if(window.pageYOffset >= sectionTop - sectionHeight/4) {
             current = section.getAttribute("id");
         }
-
-
-        menus.forEach(menu=>{
-            menu.classList.remove("nav__menu--foused");
-            const href = menu.getAttribute("href").substring(1);
-            if(href===current) {
-                //현재 있는 영역의 id와 메뉴의 링크주소가 일치할때
-                //When the Id of the current section matches the href of the menu
-                menu.classList.add("nav__menu--foused");
-                setMarker(menu);
+        if(height <=contactBox.clientHeight/2) {
+            current = section.getAttribute("id");
+        }
+        // 섹션 등장 애니메이션
+        if(current=="profile") {
+            document.querySelector(".greeting").classList.add("focus-in-expand");
+            document.querySelector(".profile__introduction").classList.add("tracking-in-contract");
+        }
+        if(current=="contact") {
+            document.querySelector(".choco-img").classList.add("slide-in-left");
+            document.querySelector(".box_contact > .category-title").classList.add("slide-in-left");
+            if(matchMedia("screen and (max-width: 757px)").matches) {
+                document.querySelector(".contact__address").classList.add("rotate-mobile");
+            }else {
+                document.querySelector(".contact__address").classList.add("rotate-in-2-br-cw");
             }
-        })
+        }
+        if(current=="work"){
+            const boxes = document.querySelectorAll(".box_work")
+            boxes.forEach(box=>{
+                const boxTop = window.pageYOffset + box.getBoundingClientRect().top;
+                const boxHeight = box.clientHeight;
+                if(window.pageYOffset >= boxTop - boxHeight*1.5) {
+                    box.classList.add("slide-in-right");
+                }
+            })
+        }
+    })
+
+    // 스크롤에 따라 해당하는 메뉴 밑에 인디케이터가 따라오는 함수
+    menus.forEach(menu => {
+        menu.classList.remove("current-menu");
+        const href = menu.getAttribute("href").substring(1);
+        if(href === current){
+            menu.classList.add("current-menu")
+            indicator(menu);
+        }
+    })
+})
+
+
+// 스크롤 시 내비게이션 컨테이너가 생기는 이벤트
+window.addEventListener("scroll", function(){
+    nav.classList.toggle("nav_fixed",window.scrollY > 0);
+})
+
+// 갤러리 바닐라 틸트
+VanillaTilt.init(document.querySelectorAll(".gallery__thumbnail")), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1
+};
+
+//이동
+const workThumbs = document.querySelectorAll(".work__thumbnail");
+workThumbs.forEach(thumb => {
+    thumb.addEventListener("click", function(e){
+        e.preventDefault();
+        window.open(this.href);
+        
     })
 })
