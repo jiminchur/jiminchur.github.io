f5f400accea9e456e3f09331c0175a76786cca66
jiminchur <ttlcc13@naver.com>
2024-07-03 01:34:12 +0900

    [중간 commit] / 수정 / 수정사항 중간 commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
+          favicon: "./content/images/main_img3.png", // Path is relative to the root
           siteName: "My Minimal Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
