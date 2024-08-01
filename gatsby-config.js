afccced0debfa8814e549902fac089eb23a73a4d
jiminchur <ttlcc13@naver.com>
2024-08-01 10:08:26 +0900

    google analysist

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,11 +21,11 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        // googleAnalytics: {
-        //     trackingId: "UA-XXXXXX-X",
-        //     anonymize: true, // Default true
-        //     environments: ["production", "development"] // Default ["production"]
-        // }
+        googleAnalytics: {
+            trackingId: "G-QS6RFTZQMQ",
+            anonymize: true, // Default true
+            environments: ["production", "development"] // Default ["production"]
+        }
       },
     },
     {
